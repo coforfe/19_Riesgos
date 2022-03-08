471e523e0b8f7f824a039077129d13d73506f6c8
Carlos Ortega <coforfe@users.noreply.github.com>
2022-03-09 00:24:14 +0100

    Remove not needed columns.

--- 00_ImportAll_.R
+++ 00_ImportAll_.R
@@ -43,18 +43,6 @@ file_list <- file_list[ !str_detect(file_list, "~")]
 file_list <- file_list[ !str_detect(file_list, "^2022")] 
 
 
-# #--- Function to read each file (all sheets)
-# read_excel_allsheets <- function(filename, tibble = FALSE) {
-#   # I prefer straight data.frames
-#   # but if you like tidyverse tibbles (the default with read_excel)
-#   # then just pass tibble = TRUE
-#   sheets <- readxl::excel_sheets(filename)
-#   x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X, skip = 13))
-#   if (!tibble) x <- lapply(x, as.data.frame)
-#   names(x) <- sheets
-#   x
-# }
-
 #---- Put all files together.
 tic()
 allrisk <- data.frame()

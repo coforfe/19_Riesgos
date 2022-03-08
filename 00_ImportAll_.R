28fed6dd88b86934571f3e720a1f12f41616b2ce
Carlos Ortega <coforfe@users.noreply.github.com>
2022-03-09 00:23:46 +0100

    Read all risk files. Correct in the process not present column "fecha_anulacion".

--- 00_ImportAll_.R
+++ 00_ImportAll_.R
@@ -43,24 +43,65 @@ file_list <- file_list[ !str_detect(file_list, "~")]
 file_list <- file_list[ !str_detect(file_list, "^2022")] 
 
 
-#--- Function to read each file (all sheets)
-read_excel_allsheets <- function(filename, tibble = FALSE) {
-  # I prefer straight data.frames
-  # but if you like tidyverse tibbles (the default with read_excel)
-  # then just pass tibble = TRUE
-  sheets <- readxl::excel_sheets(filename)
-  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X, skip = 13))
-  if (!tibble) x <- lapply(x, as.data.frame)
-  names(x) <- sheets
-  x
-}
+# #--- Function to read each file (all sheets)
+# read_excel_allsheets <- function(filename, tibble = FALSE) {
+#   # I prefer straight data.frames
+#   # but if you like tidyverse tibbles (the default with read_excel)
+#   # then just pass tibble = TRUE
+#   sheets <- readxl::excel_sheets(filename)
+#   x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X, skip = 13))
+#   if (!tibble) x <- lapply(x, as.data.frame)
+#   names(x) <- sheets
+#   x
+# }
 
-allrisk <- data.table()
+#---- Put all files together.
+tic()
+allrisk <- data.frame()
 for (i in 1:length(file_list)) {
   
+  print(file_list[i])
   filetmp    <- paste(riskdir, file_list[i], sep = "")
-  sheetstmp  <- read_excel_allsheets(filetmp)
-  allrisktmp <- rbindlist(sheetstmp, idcol = TRUE)
-  allrisk    <- rbind(allrisk, allrisktmp)
+  sheetstmp  <- readxl::excel_sheets(filetmp) 
+  
+  # there are inconsistencies in names between sheets. Missing column in 2019 and part of
+  # 2020 colum "Fecha anulación". Check if it exists, if not add it, since now it is the default. 
+  filepile <- data.frame() 
+  for (j in 1:length(sheetstmp)) {
+    
+    print(c(i, j, sheetstmp[j]))
+    tmpsheet <- readxl::read_excel(filetmp, sheet = sheetstmp[j], skip = 13) %>%
+      as.data.frame() %>%
+      clean_names()
+    namtmp   <- names(tmpsheet)
+    # print(namtmp)
+    
+    if ( !("fecha_anulacion" %in% namtmp) ) {
+      tmpsheet %<>%
+        mutate( fecha_anulacion = 0) %>%
+        relocate.( fecha_anulacion, .before = localidad) %>%
+        mutate.( midate = stri_trans_tolower(sheetstmp[j])) %>%
+        as.data.frame()
+      
+      # namend <- names(tmpsheet)
+        filepile <- rbind(filepile, tmpsheet)
+        # print(namend)
+    } else {
+        tmpsheet %<>%
+           mutate.( midate = stri_trans_tolower(sheetstmp[j])) %>%
+           as.data.frame() %>% 
+           clean_names()
+      
+        filepile <- rbind(filepile, tmpsheet)
+        # print(namend)
+    }
+  }
+  
+  allrisk    <- rbind(allrisk, filepile)
   
 }
+toc()
+
+#- Clean environment
+rm(filepile, tmpsheet, i, j, namtmp, sheetstmp, filetmp, file_list, riskdir)
+tend <- Sys.time(); tend - tini
